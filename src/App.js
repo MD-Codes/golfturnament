782ca21514e38922c6e7ab0882c4f662d074a3ca
MD-Code <mat.daszkiewicz@gmail.com>
2022-01-20 16:29:02 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
+import { useState, useEffect } from 'react';
+import React from 'react';
+import { io } from "socket.io-client";
 import './App.css';
 
+
 function App() {
+  const [args, setArgs] = useState([]);
+  const socket = io("https://mst-full-stack-dev-test.herokuapp.com/");
+
+  useEffect(() => {
+    socket.on('data-update', (...args) => {
+    setArgs(args[0]);
+    },[args]);
+    
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <tr>
+          <td>First: </td>
+          <td>{args.First}</td>
+        </tr>
+        <tr>
+          <td>Last: </td>
+          <td>{args.Last}</td>
+        </tr>
+      </table>
     </div>
-  );
+  )
+
 }
 
 export default App;
+
