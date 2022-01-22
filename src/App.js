3dc55636c4323e6c87679028e3a1adb698131560
MD-Code <mat.daszkiewicz@gmail.com>
2022-01-22 14:09:22 +0000

    Second chance

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-
-import { useState, useEffect } from 'react';
-import React from 'react';
-import { io } from "socket.io-client";
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import { v4 as uuidv4 } from 'uuid';
+import { io } from "socket.io-client";
 
 function App() {
-  const [args, setArgs] = useState([]);
-  const socket = io("https://mst-full-stack-dev-test.herokuapp.com/");
+  const [args, setArgs] = useState({MSTID: '39271', Match: '21', First: 'Jake', Last: 'Kieffer', Nationality:  'SWE'});
+  const con = io("https://mst-full-stack-dev-test.herokuapp.com/");
 
-  useEffect(() => {
-    socket.on('data-update', (...args) => {
-    setArgs(args[0]);
-    },[args]);
-    
+  useEffect(()=>{
+    con.once("data-update", (...player) => {
+      setArgs(player[0]);
+      }, [con.id]);
   });
-
+  
+  let details = [];
+  for( let x in args) {
+    details.push(<><td>{x}</td><td>{args[x]}</td></>);
+  }
   return (
-    <div>
+      <div>
       <table>
-        <tr>
-          <td>First: </td>
-          <td>{args.First}</td>
-        </tr>
-        <tr>
-          <td>Last: </td>
-          <td>{args.Last}</td>
-        </tr>
+        <tbody>
+        {details.map(detail => (
+          <tr key={uuidv4()}>{detail}</tr>
+        ))}
+        </tbody>
       </table>
-    </div>
+      </div>
   )
-
 }
 
 export default App;
